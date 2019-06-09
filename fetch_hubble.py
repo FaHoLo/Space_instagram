eb8cfb81ce743d83837c96954b59b12627c9c5f1
FaHoLo <47195343+FaHoLo@users.noreply.github.com>
2019-06-09 21:50:31 +0300

    adds: ifmain, splitext, http check

--- fetch_hubble.py
+++ fetch_hubble.py
@@ -1,12 +1,21 @@
 import requests
 import pathlib
+import os
 import json
 
 
+def main():
+    pathlib.Path('images').mkdir(parents=True, exist_ok=True)
+    fetch_hubble_collection('wallpaper')
+
 def download_image(url, image_name):
     response = requests.get(url)
-    with open(f'images/{image_name}', 'wb') as new_file:
-        new_file.write(response.content)
+    if response.ok:
+        with open(f'images/{image_name}', 'wb') as new_file:
+            new_file.write(response.content)
+    else:
+        with open(f'images/{image_name}.txt', 'w') as new_file:
+            new_file.write(f'{response}, {response.content}')
 
 def get_link_file_extension(link):
     extansion = link.split('.')[-1]
@@ -14,17 +23,27 @@ def get_link_file_extension(link):
 
 def fetch_hubble_photo(id):
     url = f'http://hubblesite.org/api/v3/image/{id}'
-    last_image_link = requests.get(url).json()['image_files'][-1]['file_url']
-    image_extension = get_link_file_extension(last_image_link)
-    image_name = f'hubble_{id}.{image_extension}'
-    download_image(last_image_link, image_name)
+    response = requests.get(url)
+    if last_image_link.ok:
+        last_image_link = response.json()['image_files'][-1]['file_url']
+        image_extension = os.path.splitext(last_image_link)[-1]
+        image_name = f'hubble_{id}{image_extension}'
+        download_image(last_image_link, image_name)
+    else:
+        with open(f'hubble_{id}.txt', 'w') as new_file:
+            new_file.write(f'{response}, {response.content}')
 
 def fetch_hubble_collection(collections_name):
     url = f'http://hubblesite.org/api/v3/images/{collections_name}'
-    collection = requests.get(url).json()
-    for image_number, image in enumerate(collection):
-        fetch_hubble_photo(image['id'])
-        print(f'{image_number + 1} of {len(collection)}')
+    response = requests.get(url)
+    if response.ok:
+        collection = response.json()
+        for image_number, image in enumerate(collection):
+            fetch_hubble_photo(image['id'])
+            print(f'{image_number + 1} of {len(collection)}')
+    else:
+        with open(f'hubble_collection.txt', 'w') as new_file:
+            new_file.write(f'{response}, {response.content}')
 
-pathlib.Path('images').mkdir(parents=True, exist_ok=True)
-fetch_hubble_collection('wallpaper')
+if __name__ == '__main__':
+    main()
--- fetch_spacex.py
+++ fetch_spacex.py
@@ -3,16 +3,29 @@ import pathlib
 import json
 
 
+def main():
+    pathlib.Path('images').mkdir(parents=True, exist_ok=True) 
+    fetch_spacex_last_launch()
+
 def download_image(url, image_name):
     response = requests.get(url)
-    with open(f'images/{image_name}', 'wb') as new_file:
-        new_file.write(response.content)
+    if response.ok:
+        with open(f'images/{image_name}', 'wb') as new_file:
+            new_file.write(response.content)
+    else:
+        with open(f'images/{image_name}.txt', 'w') as new_file:
+            new_file.write(f'{response}, {response.content}')
 
 def fetch_spacex_last_launch():
     url = 'https://api.spacexdata.com/v3/launches/65'
-    spacex_links = requests.get(url).json()['links']['flickr_images']
-    for link_number, link in enumerate(spacex_links):
-        download_image(link, f'spacex_{link_number}.jpg')
+    response = requests.get(url)
+    if response.ok:
+        spacex_links = response.json()['links']['flickr_images']
+        for link_number, link in enumerate(spacex_links):
+            download_image(link, f'spacex_{link_number}.jpg')
+    else:
+        with open(f'images/spacex_lust_launch.txt', 'w') as new_file:
+            new_file.write(f'{response}, {response.content}')
 
-pathlib.Path('images').mkdir(parents=True, exist_ok=True) 
-fetch_spacex_last_launch()
+if __name__ == '__main__':
+    main()
