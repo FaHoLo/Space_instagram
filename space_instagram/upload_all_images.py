9dda71eb63f6ecedd991857ca3659fceea06821f
FaHoLo <47195343+FaHoLo@users.noreply.github.com>
2019-06-11 19:50:12 +0300

    adds os.path.join & functions, move pyfiles
    1. adds os.path.join everywhere 2. adds new functions in upload_... .py
    3. move all pyfiles to folder

--- space_instagram/upload_all_images.py
+++ space_instagram/upload_all_images.py
@@ -1,28 +1,34 @@
 import os
 from instabot import Bot
+bot = Bot()
 from dotenv import load_dotenv
 load_dotenv()
 from PIL import Image
 
 
 def main():
+    log_into_insta()
+    upload_images_from_folder('images')
+
+def log_into_insta():
     INSTA_LOGIN = os.getenv('INSTA_LOGIN')
     INSTA_PASSWORD = os.getenv('INSTA_PASSWORD')
-    bot = Bot()
     bot.login(username=INSTA_LOGIN, password=INSTA_PASSWORD)
-    images_name = os.listdir('images')
+
+def upload_images_from_folder(folder_name):
+    images_name = os.listdir(folder_name)
     for image_number, image_name in enumerate(images_name):
         if not image_name.endswith('.jpg') or not image_name.endswith('.jpeg'):
-            image = Image.open(f'images/{image_name}')
+            image = Image.open(os.path.join('images', f'{image_name}'))
             rgb_image = image.convert('RGB')
-            rgb_image.save(f'images/{image_name}.jpg')
+            rgb_image.save(os.path.join('images', f'{image_name}.jpg'))
             upload_image_to_insta(f'{image_name}.jpg')
         else:
             upload_image_to_insta(image_name)  
         print(f'{image_number + 1} of {len(images_name)} images uploaded')
 
 def upload_image_to_insta(image_name):
-    bot.upload_photo(f'images/{image_name}', caption=f'Here is {image_name} image')
+    bot.upload_photo(os.path.join('images', f'{image_name}'), caption=f'Here is {image_name} image')
 
 if __name__ == '__main__':
     main()
